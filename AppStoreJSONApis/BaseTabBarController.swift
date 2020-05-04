6f013a855528c8b526e3f0e06395246de3d96236
Joo Hee Kim <imjhk03@gmail.com>
2020-05-05 04:00:02 +0900

    Initial commit

---
+++ AppStoreJSONApis/BaseTabBarController.swift
@@ -0,0 +1,41 @@
+//
+//  BaseTabBarController.swift
+//  AppStoreJSONApis
+//
+//  Created by Joo Hee on 2020/05/05.
+//  Copyright © 2020 Joo Hee. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let redViewController = UIViewController()
+        redViewController.view.backgroundColor = .white
+        redViewController.navigationItem.title = "Apps"
+        
+        let redNavController = UINavigationController(rootViewController: redViewController)
+        redNavController.tabBarItem.title = "Apps"
+        redNavController.tabBarItem.image = #imageLiteral(resourceName: "apps")
+        redNavController.navigationBar.prefersLargeTitles = true
+        
+        let blueViewController = UIViewController()
+        blueViewController.view.backgroundColor = .white
+        blueViewController.navigationItem.title = "Search"
+        
+        let blueNavController = UINavigationController(rootViewController: blueViewController)
+        blueNavController.tabBarItem.title = "Search"
+        blueNavController.tabBarItem.image = UIImage(named: "search")
+        blueNavController.navigationBar.prefersLargeTitles = true
+        
+        viewControllers = [
+            redNavController,
+            blueNavController
+        ]
+        
+    }
+    
+}
---
+++ AppStoreJSONApis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreJSONApis
+//
+//  Created by Joo Hee on 2020/05/05.
+//  Copyright © 2020 Joo Hee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
