83e7ec1433628783dc257a5a69ae5c4745046423
Joo Hee Kim <imjhk03@gmail.com>
2020-05-05 16:32:01 +0900

    iTunes Search API JSON Fetch

---
+++ AppStoreJSONApis/Model/SearchResult.swift
@@ -0,0 +1,19 @@
+//
+//  SearchResult.swift
+//  AppStoreJSONApis
+//
+//  Created by Joo Hee on 20. 05. 05..
+//  Copyright © 2020 Joo Hee. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Decodable {
+    let resultCount: Int
+    let results: [Result]
+}
+
+struct Result: Decodable {
+    let trackName: String
+    let primaryGenreName: String
+}
