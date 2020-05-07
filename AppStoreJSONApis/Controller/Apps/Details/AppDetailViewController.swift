1814f58c20f8873d9141469e9df58b7fa10b0fb6
Joo Hee Kim <imjhk03@gmail.com>
2020-05-08 02:20:34 +0900

    JSON Custom CodingKeys

--- AppStoreJSONApis/Controller/Apps/Details/AppDetailViewController.swift
+++ AppStoreJSONApis/Controller/Apps/Details/AppDetailViewController.swift
@@ -94,4 +94,8 @@ class AppDetailViewController: BaseListController, UICollectionViewDelegateFlowL
         return .init(width: view.frame.width, height: height)
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 0, left: 0, bottom: 16, right: 0)
+    }
+    
 }
