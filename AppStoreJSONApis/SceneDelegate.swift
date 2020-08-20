b0530dce90fc2c85e973de00a4e6e0eea4651af0
Joo Hee Kim <imjhk03@gmail.com>
2020-08-20 13:48:02 +0900

    SectionProvider Multiple Sections and Vertical Groups

--- AppStoreJSONApis/SceneDelegate.swift
+++ AppStoreJSONApis/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = BaseTabBarController() // Place your root view controller here
+            window.rootViewController = UINavigationController(rootViewController: CompositionalController()) //BaseTabBarController() // Place your root view controller here
             self.window = window
             window.makeKeyAndVisible()
         }
--- AppStoreJSONApis/Views/Apps/AppsHeaderCell.swift
+++ AppStoreJSONApis/Views/Apps/AppsHeaderCell.swift
@@ -18,6 +18,7 @@ class AppsHeaderCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        imageView.image = #imageLiteral(resourceName: "holiday")
         companyLabel.textColor = .blue
         titleLabel.numberOfLines = 2
         
